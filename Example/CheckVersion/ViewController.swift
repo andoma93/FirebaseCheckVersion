e28f260e0ffcb1b03324245d244b0d520ed0cab1
Mario Monaco <andoma93@gmail.com>
2018-07-27 15:06:52 +0200

    Initial commit

---
+++ Example/CheckVersion/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CheckVersion
+//
+//  Created by andoma93 on 07/27/2018.
+//  Copyright (c) 2018 andoma93. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
